c4e789f0d6e56f6b00b16d5ffd7491306d2b4a56
이귀현 <dlrnlgus7660@naver.com>
2024-01-26 17:16:26 +0900

    [feat] 코드아레나 메인페이지 UI 구현 및 Chat 시스템 테스트

---
+++ front-end/src/components/arena/Statsistics.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function StatsisticsPage() {
+
+  return (
+    <div className="flex flex-col mt-5 relative">
+      <br />
+      <h1 className='ml-5 font-bold'>나의 전적</h1>
+    </div>
+  );
+}
